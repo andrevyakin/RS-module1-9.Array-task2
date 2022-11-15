a53741b037083f882d5f72ad35f7a1986080f0a4
Андрей <andrevyakin@gmail.com>
2022-11-15 12:45:03 +0300

    Another way

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Способ 1 (Строго по условию задачи)
 getSumOfSequence = number => {
     const arr = [];
     for (let i = 0; i < number; i++)
@@ -5,4 +6,11 @@ getSumOfSequence = number => {
     return arr[0] + arr[arr.length - 1];
 }
 
+//Способ 2
+getSumOfSequence2 = number => [...Array(number).keys()]
+    .map(i => ++i)
+    .filter((el, i) => (i === 0 || i === number - 1))
+    .reduce((acc, el) => acc+el)
+
 console.log(getSumOfSequence(5))
+console.log(getSumOfSequence2(5))
